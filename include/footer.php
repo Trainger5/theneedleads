141dc05bb1a7c1fdeb3b2a90ce8e8b4d54a39a2b
pjthakur04ch-blip <pjthakur04ch@gmail.com>
2025-12-31 12:49:24 +0530

    animation added to index page

--- include/footer.php
+++ include/footer.php
@@ -436,3 +436,12 @@ function makeCallOrWhatsApp() {
 }
 </script>
 
+<script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+<script>
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: false,     
+    mirror: true    
+  });
+</script>
